8d5dc14bcb3e2f70566d8d830c602481feaa9bee
Graham Elozonachukwu Chukwumaobi <cheloghm@gmail.com>
2021-01-12 02:27:56 +0000

    Updated comic Book detail view

--- comicBookGallery/Controllers/ComicBooksController.cs
+++ comicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,19 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like <strong> Aldus PageMaker </strong> including versions of Lorem Ipsum.</p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colours: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
             return View();            
         }
     }
