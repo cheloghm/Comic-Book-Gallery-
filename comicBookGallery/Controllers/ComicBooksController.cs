7180c7b85794c9c53d66d8d371dee580da74ed3f
Graham Elozonachukwu Chukwumaobi <cheloghm@gmail.com>
2021-01-12 14:24:12 +0000

    Updated Comic Book Detail View to be strongly typed

--- comicBookGallery/Controllers/ComicBooksController.cs
+++ comicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,19 +12,24 @@ namespace ComicBookGallery.Controllers
         public ActionResult Detail()
         {
 
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like <strong> Aldus PageMaker </strong> including versions of Lorem Ipsum.</p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colours: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like <strong> Aldus PageMaker </strong> including versions of Lorem Ipsum.</p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script"},
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils"},
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks"},
+                    new Artist() { Name = "Edgar Delgado", Role = "Colours"},
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters"},
+                }
             };
 
-            return View();            
+            //ViewBag.ComicBook = comicBook;
+
+            return View(comicBook);            
         }
     }
 }
--- comicBookGallery/Models/ComicBook.cs
+++ comicBookGallery/Models/ComicBook.cs
@@ -1,10 +1,9 @@
-﻿using ComicBookGallery.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ComicBookGallery.Images
+namespace ComicBookGallery.Models
 {
     public class ComicBook
     {
