948a6c9e0daff09b26d081ed7f95e3f34b21c4b1
Graham Elozonachukwu Chukwumaobi <cheloghm@gmail.com>
2021-01-12 16:05:03 +0000

    Added Comic Book Repository

---
+++ comicBookGallery/Data/ComicBookRepository.cs
@@ -0,0 +1,77 @@
+﻿using ComicBookGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Data
+{
+    public class ComicBookRepository
+    {
+        private static ComicBook[] _comicBooks = new ComicBook[]
+        {
+            new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like <strong> Aldus PageMaker </strong> including versions of Lorem Ipsum.</p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script"},
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils"},
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks"},
+                    new Artist() { Name = "Edgar Delgado", Role = "Colours"},
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters"},
+                },
+                Favourite = true
+            },
+            new ComicBook()
+            {
+                SeriesTitle = "Bone",
+                IssueNumber = 50,
+                DescriptionHtml = "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like <strong> Aldus PageMaker </strong> including versions of Lorem Ipsum.</p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Graham Chukwumaobi", Role = "Script"},
+                    new Artist() { Name = "Graham Chukwumaobi", Role = "Pencils"},
+                    new Artist() { Name = "Graham Chukwumaobi", Role = "Inks"},
+                    new Artist() { Name = "Graham Chukwumaobi", Role = "Colours"},
+                    new Artist() { Name = "Graham Chukwumaobi", Role = "Letters"},
+                },
+                Favourite = false
+            },
+            new ComicBook()
+            {
+                SeriesTitle = "Amazing Spider-Man",
+                IssueNumber = 50,
+                DescriptionHtml = "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like <strong> Aldus PageMaker </strong> including versions of Lorem Ipsum.</p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Graham Chukwumaobi", Role = "Script"},
+                    new Artist() { Name = "Graham Chukwumaobi", Role = "Pencils"},
+                    new Artist() { Name = "Graham Chukwumaobi", Role = "Inks"},
+                    new Artist() { Name = "Graham Chukwumaobi", Role = "Colours"},
+                    new Artist() { Name = "Graham Chukwumaobi", Role = "Letters"},
+                },
+                Favourite = false
+            },
+
+    };
+        public ComicBook GetComicBook(int id)
+        {
+            ComicBook comicBookToReturn = null;
+
+            foreach (var comicBook in _comicBooks)
+            {
+                if (comicBook.Id == id)
+                {
+                    comicBookToReturn = comicBook;
+
+                    break;
+                }
+            }
+
+            return comicBookToReturn;
+        }
+    }
+}
