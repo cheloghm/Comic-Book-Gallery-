d03e9b22b654b3cff3e511ee627642bb6efe3695
Graham Elozonachukwu Chukwumaobi <cheloghm@gmail.com>
2021-01-12 13:55:03 +0000

    Added Comic Book and Artist Models

---
+++ comicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,19 @@
+﻿using ComicBookGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Images
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favourite { get; set; }
+
+    }
+}
